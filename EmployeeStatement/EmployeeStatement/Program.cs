60d2cb9c901075c8341a2b7712f60f5f77f71aed
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-17 20:44:34 -0800

    [Swetha] add : ability to save total wage for company

--- EmployeeStatement/EmployeeStatement/Program.cs
+++ EmployeeStatement/EmployeeStatement/Program.cs
@@ -6,9 +6,11 @@ namespace EmployeeStatement
     {
         static void Main(string[] args)
         {
-            Employee emp = new Employee();
-            Employee.Check("WIPRO",30,20,120);
-            Employee.Check("TATA", 40, 25, 100);
+            Employee emp = new Employee("WIPRO", 30, 20, 120);
+            Employee empone = new Employee("TCS", 30, 20, 120);
+            emp.CheckEmployee();
+            Console.Write(emp.ToString());
+            Console.Write(empone.ToString());
             Console.ReadLine();
         }
     }
